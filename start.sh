1d97d7f291c23cc5c2af60bc718823a8a68a001d
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2024-06-26 21:13:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Leomessibot/leothebot.git /DQTheFileDonorBot
+  git clone https://github.com/Leomessibot/leothebot.git /leothebot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  git clone $UPSTREAM_REPO /leothebot
 fi
-cd /DQTheFileDonorBot
+cd /leothebot
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting Leo-the-Bot...."
 python3 bot.py
